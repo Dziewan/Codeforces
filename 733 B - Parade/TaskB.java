6bf9549e73f2eeae2f5364f825843f4531ef9a3d
Dziewan <dziewan7@gmail.com>
2017-03-14 16:36:46 +0100

    Update TaskB.java

--- 733 B - Parade/TaskB.java
+++ 733 B - Parade/TaskB.java
@@ -9,13 +9,19 @@ public class TaskB {
         int L = 0,   R = 0;
         int arr[] = new int[n];
         int sarr[] = new int[n];
+        
+        // Wczytywanie danych z Inputu oraz liczenie sumy obydwu tablic
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
             sarr[i] = in.nextInt();
             L += arr[i];
             R += sarr[i];
         }
+        
+        // Wyliczenie |L-R|, ustawienie initial beauty
         int max = Math.abs(L-R);
+        
+        // Wyszukiwanie najlepszej konfiguracji kolumn, dla ustawienia max beauty > initial beauty
         for (int i = 0; i < n; i++) {
             int cur = Math.abs((L-arr[i]+sarr[i]) - (R-sarr[i]+arr[i]));
             if (cur > max) {
